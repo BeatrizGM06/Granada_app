126e4705b74949b96f824d6fe02335c0634e6bda
Beatriz <beatrizgamiz660@gmail.com>
2025-09-30 20:07:02 +0200

    creadas etiquetas de la función App

---
+++ Desktop/programa/estilos/estilos/App.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <Text>Boton 1</Text>
+      </View>
+      <View>
+        <Text>Boton 2</Text>
+      </View>
+      <View>
+        <Text>Boton 3</Text>
+      </View>
+      <View>
+        <Text>Boton 4</Text>
+      </View>
+      <View>
+        <Text>Boton 5</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
