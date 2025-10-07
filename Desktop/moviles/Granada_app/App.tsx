3d6f31bf8049cb6ef36b6b9c4a3fa17867c785f5
Beatriz <beatrizgamiz660@gmail.com>
2025-10-07 20:05:56 +0200

    las mejores rutas finalizadas

--- Desktop/moviles/Granada_app/App.tsx
+++ Desktop/moviles/Granada_app/App.tsx
@@ -1,13 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import globalStyles from './styles/globalstyles'
-import { Image } from 'expo-image'
+import { Image, ImageBackground } from 'expo-image'
 import { ScrollView } from 'react-native'
+import { useFonts } from 'expo-font';
 
 export default function App() {
   const COLOR_FONDO="#121212"
   const COLOR_TITULO="#ffdd99"
   const COLOR_TEXTO_FOTO="#ffffff"
+
+  useFonts({
+    "bebasNeue":require("./assets/BebasNeue-Regular.ttf")
+  });
+
   return (
     <View style={[globalStyles.contenedorPrincipal, {backgroundColor: COLOR_FONDO}]}>
       <ScrollView>
@@ -17,12 +23,24 @@ export default function App() {
           <Text style={[globalStyles.titulo,{color:COLOR_TITULO}]}>¿Qué hacer en Granada?</Text>
           <ScrollView>
             <ScrollView horizontal={true}>
-              <Image source={require("./assets/actividad1.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}></Image>
-              <Image source={require("./assets/actividad2.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}></Image>
-              <Image source={require("./assets/actividad3.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}></Image>
-              <Image source={require("./assets/actividad4.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}></Image>
-              <Image source={require("./assets/actividad5.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}></Image>
+              <Image source={require("./assets/actividad1.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}/>
+              <Image source={require("./assets/actividad2.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}/>
+              <Image source={require("./assets/actividad3.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}/>
+              <Image source={require("./assets/actividad4.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}/>
+              <Image source={require("./assets/actividad5.jpg")} contentFit='contain' style={[globalStyles.fotoCarrusel]}/>
             </ScrollView>
+            
+            <Text style={[globalStyles.titulo,{color:COLOR_TITULO}]}>Las mejores rutas</Text>
+            <ImageBackground source={require("./assets/mejores1.jpg")} contentFit='contain' style={[globalStyles.fotoRuta]}>
+              <Text style={[globalStyles.textoFoto,{color:COLOR_TEXTO_FOTO}]}>Albaicín</Text>
+            </ImageBackground>
+            <ImageBackground source={require("./assets/mejores2.jpg")} contentFit='contain' style={[globalStyles.fotoRuta]}>
+              <Text style={[globalStyles.textoFoto,{color:COLOR_TEXTO_FOTO}]}>Sacromonte</Text>
+            </ImageBackground>
+            <ImageBackground source={require("./assets/mejores3.jpg")} contentFit='contain' style={[globalStyles.fotoRuta]}>
+              <Text style={[globalStyles.textoFoto,{color:COLOR_TEXTO_FOTO}]}>El centro</Text>
+            </ImageBackground>
+
           </ScrollView>
 
           
--- Desktop/moviles/Granada_app/styles/globalstyles.ts
+++ Desktop/moviles/Granada_app/styles/globalstyles.ts
@@ -21,6 +21,27 @@ const globalStyles = StyleSheet.create({
     marginRight:10,
     borderRadius:10,
   },
+  fotoRuta:{
+    width:'100%',
+    height:200,
+    marginVertical:5,
+  },
+  textoFoto:{
+    margin:"auto",
+    fontSize:48,
+    fontFamily:"bebasNeue",
+    backgroundColor:"rgba(0, 0, 0, 0.01)",
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
 })
 
 export default globalStyles
