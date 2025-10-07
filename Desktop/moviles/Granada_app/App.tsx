7b6d06369ddec369c73932a4bbf366381f2d67ce
Beatriz <beatrizgamiz660@gmail.com>
2025-10-07 18:22:42 +0200

    cabecera finalizada

--- Desktop/moviles/Granada_app/App.tsx
+++ Desktop/moviles/Granada_app/App.tsx
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import globalStyles from './styles/globalstyles'
+import { ScrollView } from 'react-native/types_generated/index'
+import { Image } from 'expo-image'
 
 export default function App() {
+  const COLOR_FONDO="#121212"
+  const COLOR_TITULO="#ffdd99"
+  const COLOR_TEXTO_FOTO="#ffffff"
   return (
-    <View>
-      
+    <View style={[globalStyles.contenedorPrincipal, {backgroundColor: COLOR_FONDO}]}>
+      <ScrollView>
+        <Image source={".assets.granada.jpg"}></Image>
+      </ScrollView>
     </View>
   )
 }
---
+++ Desktop/moviles/Granada_app/styles/globalstyles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+  contenedorPrincipal:{
+    flex:1,
+    flexDirection:'column',
+  }
+})
+
+export default globalStyles
